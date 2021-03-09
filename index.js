c7ef99ac847c315ddf7c0b1e79f8531521169b6d
loChris <44450518+loChris@users.noreply.github.com>
2021-03-09 13:45:31 -0400

    connected/tested db

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console.error, 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected...');
+});
 
 const app = express();
 const PORT = 3000;
@@ -7,8 +21,14 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-	res.render('home');
+app.get('/makecampground', async (req, res) => {
+	const camp = new Campground({
+		title: 'My Backyard',
+		description: 'Cheap Camping',
+	});
+
+	await camp.save();
+	res.send(camp);
 });
 
 app.listen(PORT, () => {
