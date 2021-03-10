c8b2d579349e4702c9a51a8f0655bff05a377cb1
loChris <44450518+loChris@users.noreply.github.com>
2021-03-10 16:16:22 -0400

    added bootstrap cards to the index

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,7 @@ const seedDB = async () => {
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
 			image: 'https://source.unsplash.com/collection/483251',
-			despcription:
+			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor.',
 			price: price,
 		});
