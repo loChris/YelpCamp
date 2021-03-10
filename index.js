0e6cd58a198928b7205f631d23c37385311a480b
loChris <44450518+loChris@users.noreply.github.com>
2021-03-10 13:27:01 -0400

    added 404 middleware

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const app = express();
+const db = mongoose.connection;
+const PORT = 3000;
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -10,21 +14,17 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useUnifiedTopology: true,
 });
 
-const db = mongoose.connection;
-
 db.on('error', console.error.bind(console.error, 'connection error:'));
 db.once('open', () => {
 	console.log('Database connected...');
 });
-
-const app = express();
-const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
 	res.render('home');
@@ -68,6 +68,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
 	const { id } = req.params;
 	await Campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
+});
+
+app.use((req, res) => {
+	res.status(404).send('Not Found');
 });
 
 app.listen(PORT, () => {
