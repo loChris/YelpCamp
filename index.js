2987b667b78708300606b5d2ec7b87e94f068c45
loChris <44450518+loChris@users.noreply.github.com>
2021-04-11 13:16:09 -0400

    Added custom helmet content security policy

--- index.js
+++ index.js
@@ -23,6 +23,31 @@ const app = express();
 const db = mongoose.connection;
 const PORT = 3000;
 
+const scriptSrcUrls = [
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://kit.fontawesome.com/',
+	'https://cdnjs.cloudflare.com/',
+	'https://cdn.jsdelivr.net',
+];
+const styleSrcUrls = [
+	'https://kit-free.fontawesome.com/',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.mapbox.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://fonts.googleapis.com/',
+	'https://use.fontawesome.com/',
+	'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css',
+];
+const connectSrcUrls = [
+	'https://api.mapbox.com/',
+	'https://a.tiles.mapbox.com/',
+	'https://b.tiles.mapbox.com/',
+	'https://events.mapbox.com/',
+];
+const fontSrcUrls = [];
+
 const sessionConfig = {
 	name: '__mtfbw.session',
 	secret: 'thisshouldbeabettersecret',
@@ -61,8 +86,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(mongoSanitize());
 app.use(
-	helmet({
-		contentSecurityPolicy: false,
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", 'blob:'],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/dxlpc7ymd/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				'https://images.unsplash.com/',
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+		},
 	})
 );
 
