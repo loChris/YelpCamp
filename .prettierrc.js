02c405edbcd31f2fe16ce7e537bec1dc2d749b3d
Chris Lopez <44450518+loChris@users.noreply.github.com>
2024-04-12 19:21:48 -0400

    pushing up prettier rc

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  quoteProps: 'as-neded',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'aways',
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  // defaults
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitiviy: 'css'
+}
